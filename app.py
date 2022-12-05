c1107031818f9a7971fc46a6336cc3b820e0882d
fran <jane08150020@163.com>
2022-12-04 19:21:59 -0800

    2nd commit

--- app.py
+++ app.py
@@ -9,18 +9,44 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 """turn the redicrect message off"""
 debug = DebugToolbarExtension(app)
 
-
-@app.route('/survey')
-def surve_page():
-    print(satisfaction_survey)
-    return render_template('survey.html', responses=responses)
-
-
-@app.route('/survey/result', methods=['POST'])
-def show_result():
-    answer = request.form['answer']
-    responses.append(answer)
-    return redirect('/survey')
-
-
 responses = []
+title = satisfaction_survey.title
+instructions = satisfaction_survey.instructions
+question_list = satisfaction_survey.questions
+
+
+@app.route('/questions')
+def survey_questions():
+    return render_template('start.html', title=title, instructions=instructions)
+
+
+@app.route('/questions/0')
+def question_0():
+    return render_template('question_0.html', question_list=question_list)
+
+
+@app.route('/questions/1')
+def question_1():
+    return render_template('question_1.html', question_list=question_list)
+
+
+@app.route('/questions/2')
+def question_2():
+    return render_template('question_2.html', question_list=question_list)
+
+
+@app.route('/questions/3')
+def question_3():
+    return render_template('question_3.html', question_list=question_list)
+
+
+@app.route('/thanks')
+def thank_you_page():
+    return render_template('thanks.html')
+
+
+@app.route('/answer')
+def collect_answer():
+    answer_0 = request.form['options']
+    responses.add(answer_0)
+    return render_template('answer.html', responses=responses)
